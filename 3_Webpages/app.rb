dded4cd6db11f10d737a186ee449e34825dd1273
Samir70 <sbetmouni@talktalk.net>
2022-11-28 17:10:02 +0000

    using interpolation to get dynamic HTML

--- 3_Webpages/app.rb
+++ 3_Webpages/app.rb
@@ -1,4 +1,5 @@
 require "sinatra/base"
+require 'sinatra/reloader'
 
 class Application < Sinatra::Base
   configure :development do
@@ -9,6 +10,7 @@ class Application < Sinatra::Base
     # The erb method takes the view file name (as a Ruby symbol)
     # and reads its content so it can be sent
     # in the response.
+    @name = params[:name]
     return erb(:index)
   end
 end
